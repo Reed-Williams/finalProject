0cc91034537d3838979f3c0eae0ea07f7287770e
Reed-Williams <williamc18@newpaltz.edu>
2022-05-02 18:04:59 -0400

    fixed typo

--- client/src/components/colorTaskView.vue
+++ client/src/components/colorTaskView.vue
@@ -12,9 +12,9 @@
     <tr>
         <td><p v-if="task.completed"><i class="fa-regular fa-square-check"></i></p></td>
         <td>{{task.title}}</td>
-        <td v-if="task.assignedBy == session.user.username"> Me </td>
+        <td v-if="task.assignedBy == session.user!.username"> Me </td>
         <td v-else>{{task.assignedBy}}</td>
-        <td v-if="task.assignedFor == session.user.username"> Me </td>
+        <td v-if="task.assignedFor == session.user!.username"> Me </td>
         <td v-else>{{task.assignedFor}}</td>
         <td>{{new Date(task.dueDate).toDateString()}}</td> 
     </tr>
